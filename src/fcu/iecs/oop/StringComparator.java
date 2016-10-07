b6e248b887ff9f357ccda8ab2f39756a2625e1cd
user <user@FF09>
2016-10-07 09:40:01 +0800

    StringComparator

---
+++ src/fcu/iecs/oop/StringComparator.java
@@ -0,0 +1,27 @@
+package fcu.iecs.oop;
+
+import java.util.Scanner;
+
+public class StringComparator {
+	
+	
+	public static void main(String[] args) {
+	String a,b;
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Enter a string 1:");
+	a=scanner.next();
+	System.out.println("Enter a string 2:");
+	b=scanner.next();
+	
+	if(a.equalsIgnoreCase(b))
+	{
+		System.out.println("The two strings are the same.");
+	}
+	else
+	{
+		System.out.println("The two strings are not the same.");
+	}
+	
+	}
+
+}
