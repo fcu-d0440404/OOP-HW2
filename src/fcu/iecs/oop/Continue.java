8b992bcfad886b6deda770de1147167018db9719
user <user@FE09>
2016-10-14 09:43:19 +0800

    1014 Exercise2

---
+++ src/fcu/iecs/oop/Continue.java
@@ -0,0 +1,16 @@
+package fcu.iecs.oop;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < 5; i ++) {
+			if(i==0||i==2||i==4||i==5)
+			{
+				continue;
+			}
+			System.out.println(i);
+			}
+	}
+
+}
